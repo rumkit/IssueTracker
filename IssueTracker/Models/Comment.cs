98376a92f5c4e1de07c07f2ea6003971996a9726
Anton Mitsengendler <a.mic@rtc.ru>
2019-09-05 23:49:06 +0300

    scaffolded view and controllers

--- IssueTracker/Models/Comment.cs
+++ IssueTracker/Models/Comment.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace IssueTracker.Models
 {
     public class Comment
     {
         public int Id { get; set; }
+        [Required]
         public string Text { get; set; }
         public DateTime DateCreated { get; set; }
     }
--- IssueTracker/Models/Issue.cs
+++ IssueTracker/Models/Issue.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace IssueTracker.Models
     public class Issue
     {
         public int Id { get; set; }
+        [Required]
         public string Theme { get; set; }
         public string Description { get; set; }
         public DateTime DateCreated { get; set; }
